15b247eeee04f663ec4af5ec45c3a5f75622b85a
Nick Barry <niko.barry@gmail.com>
2016-05-01 20:30:41 -0700

    Add Youtube API key and relevant npm package
    Add the relevant API key in a file untracked by git, and install an NPM package that helps make Youtube searches using an API key

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import config from '../configUntracked';
+
+const API_KEY = config.YOUTUBE_API_KEY;
 
 const App = () => {
     return <div>Hi!</div>;
