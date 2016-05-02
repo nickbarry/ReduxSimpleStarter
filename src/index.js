b4eca7dcf4e45413213b1d109668027e17e769f7
Nico Barry <niko.barry@gmail.com>
2016-05-02 12:32:13 -0700

    Update display based on user click
    Now the user can click a video from the video list, and it will load that video in the video detail component.

--- src/index.js
+++ src/index.js
@@ -4,23 +4,33 @@ import config from '../configUntracked';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list'
+import VideoDetail from './components/video_detail';
 const API_KEY = config.YOUTUBE_API_KEY;
 
 class App extends Component {
     constructor(props){
         super(props);
 
-        this.state = {videos: []};
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
 
         YTSearch({key: API_KEY, term: 'parkour'},
-            (videos) => this.setState({videos}));
+            (videos) => this.setState({
+                videos,
+                selectedVideo: videos[0]
+            }));
     }
 
     render(){
         return (
             <div>
                 <SearchBar />
-                <VideoList videos={this.state.videos} />
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                    videos={this.state.videos} />
             </div>
         );
     }
