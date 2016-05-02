3995f178a97c08d3605dce4111d1b4e2fa70bd28
Nick Barry <niko.barry@gmail.com>
2016-05-01 20:34:58 -0700

    Remind myself to update configUntracked on my desktop
    I use a configUntracked.js file for storing API keys, db passwords, etc. Since I don't use git to track these files, I need to use some other system to keep the relevant files synced across computers. So I need to set myself reminders in files that **are** tracked to manually update the relevant files on other computers.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import config from '../configUntracked';
 
-const API_KEY = config.YOUTUBE_API_KEY;
+const API_KEY = config.YOUTUBE_API_KEY; // TODO: Update configUntracked.js file on desktop computer
 
 const App = () => {
     return <div>Hi!</div>;
