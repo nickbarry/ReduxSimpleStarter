179c968d655b8a25552414e80a7b0f61d63e4cf8
Nick Barry <niko.barry@gmail.com>
2016-05-01 20:45:32 -0700

    Create basic SearchBar component and include in our page
    So far the SearchBar is just an input element with nothing else going on. It displays on the homepage.

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import config from '../configUntracked';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = config.YOUTUBE_API_KEY; // TODO: Update configUntracked.js file on desktop computer
 
 const App = () => {
-    return <div>Hi!</div>;
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
 };
 
 ReactDOM.render(<App />,document.getElementsByClassName('container')[0]);
