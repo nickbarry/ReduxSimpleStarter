26e5eb44a429d23bba9e267f0fd7665342174515
Nico Barry <niko.barry@gmail.com>
2016-05-03 09:03:04 -0700

    Add throttling to searchbar changes
    No longer kick off a new API request on every keystroke/backspace; wait for 300ms of inaction before kicking off a new search.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import config from '../configUntracked';
@@ -20,7 +21,7 @@ class App extends Component {
     }
 
     videoSearch(term){
-        YTSearch({key: API_KEY, term: term},
+        YTSearch({key: API_KEY, term},
             (videos) => this.setState({
                 videos,
                 selectedVideo: videos[0]
@@ -28,9 +29,11 @@ class App extends Component {
     }
 
     render(){
+        const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
+
         return (
             <div>
-                <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
+                <SearchBar onSearchTermChange={videoSearch} />
                 <VideoDetail video={this.state.selectedVideo} />
                 <VideoList
                     onVideoSelect={selectedVideo => this.setState({selectedVideo})}
